4c2696039386da704c7b7befca0017d410cb7609
Akil0814 <akil040814@gmail.com>
2026-07-01 13:30:39 -0700

    7/1

--- gameplay/scene/startup_loading_scene.cpp
+++ gameplay/scene/startup_loading_scene.cpp
@@ -9,46 +9,6 @@
 #include <SDL_image.h>
 #include <iostream>
 
-namespace
-{
-constexpr int kLogicalWidth = 1280;
-constexpr int kLogicalHeight = 720;
-constexpr int kSplashMaxWidth = 320;
-constexpr int kSplashMaxHeight = 180;
-constexpr int kSplashMargin = 24;
-
-SDL_Rect build_bottom_right_rect(SDL_Texture* texture)
-{
-	if (!texture)
-		return SDL_Rect{ 0, 0, 0, 0 };
-
-	int texture_width = 0;
-	int texture_height = 0;
-	if (SDL_QueryTexture(texture, nullptr, nullptr, &texture_width, &texture_height) != 0
-		|| texture_width <= 0
-		|| texture_height <= 0)
-	{
-		return SDL_Rect{ 0, 0, 0, 0 };
-	}
-
-	const float width_ratio =
-		static_cast<float>(kSplashMaxWidth) / static_cast<float>(texture_width);
-	const float height_ratio =
-		static_cast<float>(kSplashMaxHeight) / static_cast<float>(texture_height);
-	const float scale = std::min(1.0f, std::min(width_ratio, height_ratio));
-
-	const int draw_width = static_cast<int>(texture_width * scale);
-	const int draw_height = static_cast<int>(texture_height * scale);
-
-	return SDL_Rect{
-		kLogicalWidth - draw_width - kSplashMargin,
-		kLogicalHeight - draw_height - kSplashMargin,
-		draw_width,
-		draw_height
-	};
-}
-}
-
 void StartUpLoadingScene::on_enter()
 {
 	_paused = false;
@@ -93,26 +53,8 @@ void StartUpLoadingScene::on_render(SDL_Renderer* renderer)
 		return;
 	}
 
-	Uint8 old_r = 0;
-	Uint8 old_g = 0;
-	Uint8 old_b = 0;
-	Uint8 old_a = 0;
-	SDL_GetRenderDrawColor(renderer, &old_r, &old_g, &old_b, &old_a);
-
-	SDL_SetRenderDrawColor(renderer, 12, 14, 20, 255);
-	SDL_Rect background{ 0, 0, kLogicalWidth, kLogicalHeight };
-	SDL_RenderFillRect(renderer, &background);
 
 	render_preload_texture(renderer);
-
-	if (_state == LoadingState::Failed)
-	{
-		SDL_SetRenderDrawColor(renderer, 180, 40, 40, 255);
-		SDL_Rect fail_bar{ 0, kLogicalHeight - 8, kLogicalWidth, 8 };
-		SDL_RenderFillRect(renderer, &fail_bar);
-	}
-
-	SDL_SetRenderDrawColor(renderer, old_r, old_g, old_b, old_a);
 }
 
 // Handles menu input, including keyboard input for starting the game.
@@ -188,10 +130,7 @@ void StartUpLoadingScene::render_preload_texture(SDL_Renderer* renderer) const
 	if (!_preload_texture || !renderer)
 		return;
 
-	const SDL_Rect destination_rect = build_bottom_right_rect(_preload_texture);
-	if (destination_rect.w <= 0 || destination_rect.h <= 0)
-		return;
-
+	const SDL_Rect destination_rect{ 886, 606, 400, 100 };
 	SDL_RenderCopy(renderer, _preload_texture, nullptr, &destination_rect);
 }
 
