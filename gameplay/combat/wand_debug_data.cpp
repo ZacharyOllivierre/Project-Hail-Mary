32261f32c57a9a561ccbac6bd98ac3cc899cf7f2
Zachary Ollivierre <zollivierre0@saddleback.edu>
2026-07-14 12:20:30 -0700

    Updated projectile debug window values

--- gameplay/combat/wand_debug_data.cpp
+++ gameplay/combat/wand_debug_data.cpp
@@ -18,7 +18,7 @@ void WandDebugData::render_debugger()
 
     ImGui::DragFloat("Cooldown", &_wand_attributes->cooldown_seconds, 0.01f, 0.0f, 30.0f);
     ImGui::DragFloat("Mana Cost", &_wand_attributes->mana_cost, 0.1f, 0.0f, 1000.0f);
-    ImGui::DragInt("Bullet Count", &_wand_attributes->bullet_count, 1.0f, 1, 128);
+    ImGui::DragInt("Bullet Count", &_wand_attributes->bullet_count, 1.0f, 1, 1000);
 
     int spread_style = static_cast<int>(_wand_attributes->spread_style);
     if (ImGui::Combo("Spread Style", &spread_style, "Uniform\0Circular\0Random\0"))
@@ -34,22 +34,22 @@ void WandDebugData::render_debugger()
         _wand_attributes->shot_style = static_cast<ShotStyle>(shot_style);
     }
 
-    ImGui::DragFloat("First Shot Delay", &_wand_attributes->first_shot_delay, 0.01f, 0.0f, 10.0f);
-    ImGui::DragFloat("Shot Delay", &_wand_attributes->shot_delay_sec, 0.01f, 0.0f, 10.0f);
+    ImGui::DragFloat("First Shot Delay", &_wand_attributes->first_shot_delay, 0.01f, 0.0f, 20.0f);
+    ImGui::DragFloat("Shot Delay", &_wand_attributes->shot_delay_sec, 0.01f, 0.0f, 20.0f);
     ImGui::DragFloat("Spawn Distance", &_wand_attributes->spawn_distance, 1.0f, 0.0f, 1000.0f);
 
     ImGui::Separator();
     ImGui::Text("Bullet Debug");
     ImGui::DragFloat("Speed", &_bullet_attributes->bullet_speed, 10.0f, 0.0f, 5000.0f);
-    ImGui::DragFloat("Acceleration", &_bullet_attributes->acceleration, 1.0f, 0.0f, 1000.0f);
+    ImGui::DragFloat("Acceleration", &_bullet_attributes->acceleration, 1.0f, 0.0f, 2000.0f);
     ImGui::DragFloat("Max Age", &_bullet_attributes->max_age, 0.1f, 0.0f, 120.0f);
     ImGui::DragFloat2("Bullet Size", &_bullet_attributes->bullet_size.x, 1.0f, 1.0f, 512.0f);
     ImGui::Checkbox("Damage Based Size", &_bullet_attributes->damage_based_size);
     ImGui::DragFloat("Damage", &_bullet_attributes->damage, 1.0f, 0.0f, 10000.0f);
 
-    ImGui::DragFloat("Curve", &_bullet_attributes->curve, 0.1f, -360.0f, 360.0f);
-    ImGui::DragInt("Bounces", &_bullet_attributes->bounces, 1.0f, 0, 32);
+    ImGui::DragFloat("Curve", &_bullet_attributes->curve, 0.1f, -3000.0f, 3000.0f);
+    ImGui::DragInt("Bounces", &_bullet_attributes->bounces, 1.0f, 0, 300);
     ImGui::DragFloat("Homing Strength", &_bullet_attributes->homing_strength, 1.0f, 0.0f, 5000.0f);
     ImGui::Checkbox("Homing Maintains Speed", &_bullet_attributes->homing_maintains_speed);
-    ImGui::DragFloat("Growth", &_bullet_attributes->growth, 0.1f, 0.0f, 1000.0f);
+    ImGui::DragFloat("Growth", &_bullet_attributes->growth, 0.1f, 0.0f, 3000.0f);
 }
